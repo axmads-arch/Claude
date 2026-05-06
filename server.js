c46a5f0b008048d15356e5f630b8422c6b3e781b
Axmad <ahmadjonabdullayev23@gmail.com>
2026-05-06 10:08:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/products', require('./routes/products'));
